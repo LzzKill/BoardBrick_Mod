0b43c0de6f5136e8e8dc017b87ebb364941ea337
fzzkill <changzhijie11@outlook.com>
2022-04-20 10:03:47 +0600

    Add a "some" blocks

--- src/main/java/com/fzz/boardbrick/ObjectRegistry.java
+++ src/main/java/com/fzz/boardbrick/ObjectRegistry.java
@@ -1,6 +1,7 @@
 package com.fzz.boardbrick;
 
 import com.fzz.boardbrick.Blocks.*;
+import com.fzz.boardbrick.Items.BoardBrick;
 import com.fzz.boardbrick.Items.BoardBrick_Food;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
@@ -8,7 +9,6 @@ import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import com.fzz.boardbrick.Items.BoardBrick;
 
 public class ObjectRegistry {
 
@@ -32,11 +32,11 @@ public class ObjectRegistry {
     public static RegistryObject<Block> C = BLOCKS.register("board_block_third", () -> new BoardBlockThird());
     public static RegistryObject<Block> D = BLOCKS.register("board_block_fourth", () -> new BoardBlockFourth());
     public static RegistryObject<Block> E = BLOCKS.register("board_block_fifth", () -> new BoardBlockFifth());
-    public static RegistryObject<Block> F = BLOCKS.register("board_block_fourth", () -> new BoardBlockSixth());
-    public static RegistryObject<Block> G = BLOCKS.register("board_block_fourth", () -> new BoardBlockSeventh());
-    public static RegistryObject<Block> H = BLOCKS.register("board_block_fourth", () -> new BoardBlockEighth());
-    public static RegistryObject<Block> I = BLOCKS.register("board_block_fourth", () -> new BoardBlockNinth());
-    public static RegistryObject<Block> J = BLOCKS.register("board_block_fourth", () -> new BoardBlockSuper());
+    public static RegistryObject<Block> F = BLOCKS.register("board_block_sixth", () -> new BoardBlockSixth());
+    public static RegistryObject<Block> G = BLOCKS.register("board_block_seventh", () -> new BoardBlockSeventh());
+    public static RegistryObject<Block> H = BLOCKS.register("board_block_eighth", () -> new BoardBlockEighth());
+    public static RegistryObject<Block> I = BLOCKS.register("board_block_ninth", () -> new BoardBlockNinth());
+    public static RegistryObject<Block> J = BLOCKS.register("board_block_super", () -> new BoardBlockSuper());
     //Registry BlockItem
 
     public static RegistryObject<Item> a = ITEMS.register("board_block_first", () -> new BlockItem(A.get(), new Item.Properties().tab(boardbrick.BoardGroup)));
@@ -44,5 +44,11 @@ public class ObjectRegistry {
     public static RegistryObject<Item> c = ITEMS.register("board_block_third", () -> new BlockItem(C.get(), new Item.Properties().tab(boardbrick.BoardGroup)));
     public static RegistryObject<Item> d = ITEMS.register("board_block_fourth", () -> new BlockItem(D.get(), new Item.Properties().tab(boardbrick.BoardGroup)));
     public static RegistryObject<Item> e = ITEMS.register("board_block_fifth", () -> new BlockItem(E.get(), new Item.Properties().tab(boardbrick.BoardGroup)));
+    public static RegistryObject<Item> f = ITEMS.register("board_block_sixth", () -> new BlockItem(F.get(), new Item.Properties().tab(boardbrick.BoardGroup)));
+    public static RegistryObject<Item> g = ITEMS.register("board_block_seventh", () -> new BlockItem(G.get(), new Item.Properties().tab(boardbrick.BoardGroup)));
+    public static RegistryObject<Item> h = ITEMS.register("board_block_eighth", () -> new BlockItem(H.get(), new Item.Properties().tab(boardbrick.BoardGroup)));
+    public static RegistryObject<Item> i = ITEMS.register("board_block_ninth", () -> new BlockItem(I.get(), new Item.Properties().tab(boardbrick.BoardGroup)));
+    public static RegistryObject<Item> j = ITEMS.register("board_block_super", () -> new BlockItem(J.get(), new Item.Properties().tab(boardbrick.BoardGroup)));
+
 
 }
